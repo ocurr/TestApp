221b62bf3fe356ffcad6c93f5e59129cdf9791cb
ocurr <owcurry42@gmail.com>
2017-06-28 13:08:44 -0700

    added prints to the tests to verify that they ran

--- TestAppTests/TestAppTests.swift
+++ TestAppTests/TestAppTests.swift
@@ -24,6 +24,7 @@ class TestAppTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        print("Hello, World\n")
         XCTAssert(1 == 1)
     }
     
