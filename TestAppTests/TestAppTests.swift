9d8d23685b7742056caff5526e89beca1033f6a6
Oliver Curry <owcurry42@gmail.com>
2017-06-30 14:12:53 -0700

    updated to swift 3

--- TestAppTests/TestAppTests.swift
+++ TestAppTests/TestAppTests.swift
@@ -30,7 +30,7 @@ class TestAppTests: XCTestCase {
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measureBlock {
+        self.measure {
             // Put the code you want to measure the time of here.
         }
     }
